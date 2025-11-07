6177be6ea8f0b307a4613b250a80e9bafc9a2183
小云朵OvO <xyswl888@gmail.com>
2025-11-08 06:44:53 +0800

    Update Info.php

--- Tokenpay/Config/Info.php
+++ Tokenpay/Config/Info.php
@@ -2,21 +2,21 @@
 declare (strict_types=1);
 
 return [
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'name' => 'TokenPay',
     'author' => 'Conan',
     'website' => '#',
-    'description' => 'TokenPay',
+    'description' => 'TokenPay加密货币支付网关',
     'options' => [
         'tokenpay' => '默认接口',
     ],
     'callback' => [
         \App\Consts\Pay::IS_SIGN => true,
         \App\Consts\Pay::IS_STATUS => true,
-        \App\Consts\Pay::FIELD_STATUS_KEY => 'status',
-        \App\Consts\Pay::FIELD_STATUS_VALUE => 2,
-        \App\Consts\Pay::FIELD_ORDER_KEY => 'order_id',
-        \App\Consts\Pay::FIELD_AMOUNT_KEY => 'amount',
+        \App\Consts\Pay::FIELD_STATUS_KEY => 'Status', 
+        \App\Consts\Pay::FIELD_STATUS_VALUE => 1, 
+        \App\Consts\Pay::FIELD_ORDER_KEY => 'OutOrderId', 
+        \App\Consts\Pay::FIELD_AMOUNT_KEY => 'ActualAmount', 
         \App\Consts\Pay::FIELD_RESPONSE => 'ok'
     ]
 ];
